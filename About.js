dc265aa028940cd4d0b89eddf93d85d2070a7e66
shobika13 <127006798+shobika13@users.noreply.github.com>
2023-09-29 20:23:42 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,5 @@
+export const About=()=>{
+    return(
+        <p>What’s your food mood today? Delicious biryani or crispy dosas, pizza or burgers, Indian sweets or cakes, chai or coffee? Whatever you feel like eating, Zomato is the only app you need for the quickest doorstep delivery or to find the best restaurants. With over 3 lakh listed restaurants, explore the best food around you with India’s most loved & trusted food app- making hungry customers happy since 2008!</p>
+    )
+};
